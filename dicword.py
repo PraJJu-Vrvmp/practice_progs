130d284d5c15a737056c0590359d423c69f4c3e1
PraJJu <prajwalvrvmp@gmail.com>
2019-01-11 15:06:27 +0530

    Program to search an element in the dictionary

---
+++ dicword.py
@@ -0,0 +1,8 @@
+dict = {1:"Speckbit", 2:"World", 3:"Quiet"}
+srch = input("Enter the word to be searched:")
+for key in dict:
+	if dict[key] == srch:
+		print (True)
+		break
+else:
+		print (False)
