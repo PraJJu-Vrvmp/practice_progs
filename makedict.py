1004ab5498d6f2b0a3f0034ef888e1bc5eb7d5b5
PraJJu <prajwalvrvmp@gmail.com>
2019-01-11 16:07:34 +0530

    Program to create a dictionary using keys and values

---
+++ makedict.py
@@ -0,0 +1,10 @@
+n = eval(input("Enter the no. of dictionary keys:"))
+lst1 = []
+lst2 = []
+for i in range(n):
+	k = input("Enter %d key:"%(i+1))
+	v = input("Enter %d value:"%(i+1))
+	lst1.append(k)
+	lst2.append(v)
+	d = dict(zip(lst1,lst2))
+print(d)
