b073d137668a0d301befda593ad55b08a89a971a
PraJJu <prajwalvrvmp@gmail.com>
2019-01-11 15:01:53 +0530

    Program to print output in multiline

---
+++ multilineprint.py
@@ -0,0 +1,5 @@
+str = "'Hello! My name is Kaustubh. What is your name?'"
+"""Input statement given to print in multiline"""
+str1 = 'Hello!\nMy name is Kaustubh.\nWhat is your name?'
+""" '\n'- Used as a newline character which prints the input statement in newlines"""
+print (str1)
