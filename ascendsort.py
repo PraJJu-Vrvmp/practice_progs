bccf6bd34f1576c086bbb3689fce343f5aa154e5
Prajwal Bharadwaj <prajwalvrvmp@gmail.com>
2019-03-15 22:26:20 +0530

    Program to sort the list in ascending order

--- ascendsort.py
+++ ascendsort.py
@@ -1,7 +1,9 @@
 lst = []
-lst.extend(eval(input("Enter elements")))
-
-for i in range(len(lst)-1):
-	if lst[i]>lst[i+1]:
-		lst[i],lst[i+1]=lst[i+1],lst[i]
+lst.extend(eval(input("Enter elements:")))
+for j in range(len(lst)-1):
+	for i in range(len(lst)-1):
+		if lst[i]>lst[i+1]:
+			temp = lst[i]
+			lst[i]=lst[i+1]
+			lst[i+1]=temp
 print(lst)
